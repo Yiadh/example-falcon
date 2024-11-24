5c4170fa571da535fd699ea2aa9a4eb908729028
i529992 <yiadh.tlijani@sap.com>
2024-11-24 11:42:23 +0100

    Avoid hardcoding Redis server address and port

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "os"
     "context"
     "fmt"
     "net/http"
@@ -11,9 +12,19 @@ var rdb *redis.Client
 var ctx = context.Background()
 
 func init() {
+    redisAddr := os.Getenv("REDIS_ADDR")
+    if redisAddr == "" {
+        redisAddr = "localhost:6379" // Default value
+    }
+
     rdb = redis.NewClient(&redis.Options{
-        Addr: "localhost:6379", // Redis server address
+        Addr: redisAddr, // Redis server address
     })
+
+    // Test Redis connection
+    if err := rdb.Ping(ctx).Err(); err != nil {
+        panic(fmt.Sprintf("Failed to connect to Redis: %v", err))
+    }
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
